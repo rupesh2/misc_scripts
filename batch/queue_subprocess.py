c5f8748c46246dbbfa86a26dc33e030bd7fc3501
Rupesh Shrestha <rvs@macwf06062.ornl.gov>
2023-08-14 12:56:47 -0400

    adding batch queue

--- batch/queue_subprocess.py
+++ batch/queue_subprocess.py
@@ -3,4 +3,4 @@
 import subprocess
 
 for j in range(1980, 1985):
-    output = subprocess.call(f"./daymet_minmax.py --year {j} | batch ", shell=True)
+    output = subprocess.Popen(f"./daymet_minmax.py --year {j} | batch ", shell=True)
